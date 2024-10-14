02dbd008d8d3e8f9bfcf83e778dd0d725ed85c87
Aimarcapo <alonso.aimar@uni.eus>
2024-10-14 08:47:35 +0200

    Pazienteak editatzeko viewa, html eta url-a sortuta

--- DjangoAzterketaAimarAlonso/Azterketa/Osakidetza/urls.py
+++ DjangoAzterketaAimarAlonso/Azterketa/Osakidetza/urls.py
@@ -4,4 +4,5 @@ from django.urls import path
 urlpatterns=[
   path('Pazientea/', views.pazienteak, name='paziente'),
   path('Pazientea/new', views.pazienteak_new, name='paziente-new'),
+  path('Pazientea/edit/<str:variable>', views.pazienteak_edit, name='paziente-edit'),
 ]
--- DjangoAzterketaAimarAlonso/Azterketa/Osakidetza/views.py
+++ DjangoAzterketaAimarAlonso/Azterketa/Osakidetza/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import redirect, render
+from django.shortcuts import get_object_or_404, redirect, render
 from .models import *
 from .forms import *
 # Create your views here.
@@ -15,3 +15,14 @@ def pazienteak_new(request):
     else:        
         form=PazienteForm()        
         return render(request, 'paziente_new.html', {'form':form})
+def pazienteak_edit(request, variable):
+    pazientea = get_object_or_404(Paziente, dni=variable)  # Obtén la nota o muestra un error 404, despues del modelo ponemos el nombre del campo de ese modelo que hemos puesto en models.py en este caso matrikula y despues del igual ponemos el nombre que se va a pasar por el enlace
+    if request.method == 'POST':
+        form = PazienteForm(request.POST, instance=pazientea)  # Crea un formulario con la instancia de la nota
+        if form.is_valid():  # Verifica si el formulario es válido
+            form.save()  # Guarda los cambios en la nota
+            return redirect('paziente')  # Redirige a la lista de notas después de editar
+    else:
+        form = PazienteForm(instance=pazientea)  # Carga el formulario con los datos actuales de la nota
+
+    return render(request, 'paziente_edit.html', {'form': form, 'pazientea': pazientea})
