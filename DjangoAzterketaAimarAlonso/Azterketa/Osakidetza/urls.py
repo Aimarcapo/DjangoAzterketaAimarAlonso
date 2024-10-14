a232a03e4692ffb702afab94b64fb000e35a2e74
Aimarcapo <alonso.aimar@uni.eus>
2024-10-14 08:38:55 +0200

    Pazientea sortzeko viewa, html eta url-a sortuta

--- DjangoAzterketaAimarAlonso/Azterketa/Osakidetza/urls.py
+++ DjangoAzterketaAimarAlonso/Azterketa/Osakidetza/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns=[
-  path('Pazientea', views.pazienteak, name='paziente'),
+  path('Pazientea/', views.pazienteak, name='paziente'),
+  path('Pazientea/new', views.pazienteak_new, name='paziente-new'),
 ]
--- DjangoAzterketaAimarAlonso/Azterketa/Osakidetza/views.py
+++ DjangoAzterketaAimarAlonso/Azterketa/Osakidetza/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from .models import *
 from .forms import *
 # Create your views here.
 def pazienteak(request):
     pazienteak=Paziente.objects.all
     return render(request, 'pazienteak.html', {'pazienteak':pazienteak })
+def pazienteak_new(request):    
+    if request.method == 'POST':        
+        form=PazienteForm(request.POST)       
+        if form.is_valid:            
+            ikasle = form.save()            
+            ikasle.save()        
+            return redirect('paziente')
+    else:        
+        form=PazienteForm()        
+        return render(request, 'paziente_new.html', {'form':form})
